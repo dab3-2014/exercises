41d767e6b5245595eef3a4e274eb53f860521108
dab3-2014 <m.cohomology@gmail.com>
2014-03-26 04:24:49 -0500

    Task2.py initial submission
    Still TO DO : 
    - Clustering with k-means, to determine the groups (clusters)
    - Latent Dirichlet Allocation with the 'gensim' package.
    - Build a Topic model.

--- ml/mywork/task2.py
+++ ml/mywork/task2.py
@@ -8,3 +8,25 @@ Building on task 1, we now want to start to understand the relationships to help
 2. What topics exist within the deals?
 
 """
+import scipy as sp
+import nltk
+import nltk.data
+import enchant
+from sklearn.feature_extraction.text import CountVectorizer
+
+d = enchant.Dict("en_US")
+vectorizer = CountVectorizer(min_df=1)
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+deals = open('../data/deals.txt','rU').read() #read deals.txt file
+sent_deals = tokenizer.tokenize(deals.strip())
+
+X = vectorizer.fit_transform(sent_deals)
+num_samples, num_features = X.shape
+print("#samples: %d, #features: %d" % (num_samples, num_features))
+#print(vectorizer.get_feature_names())
+
+# T O D O : 
+# - Clustering with k-means, to determine the groups (clusters)
+# - Latent Dirichlet Allocation with the 'gensim' package.
+# - Build a Topic model.
